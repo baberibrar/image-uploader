cd36f65f3cc2bc00ed50bff6abfb5cefd227437e
Baber Ibrar <61284156+baberibrar@users.noreply.github.com>
2023-12-28 08:28:33 +0000

    🖼️ Added 'Image' model for photo uploads

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.functions import Now, Pi
 
 # Create your models here.
+
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to="myimage")
+    date = models.DateTimeField(db_default=Now())
